bd410463bcbe6f232df74be49cbcadc06bade0d3
Garrett Lee <rgl10d@gmail.com>
2021-10-04 14:14:01 -0400

    created array of objects for all communities

--- client/src/containers/Communities/Communities.jsx
+++ client/src/containers/Communities/Communities.jsx
@@ -7,92 +7,408 @@ const communityStates = [
   {
     state: "Alabama",
     usStateId: "alabama",
-    communities: ["Alabama Community", "Alabama Community"],
+    stateAbbr: "AL",
+    communities: [
+      {
+        communityName: "Wolf Bay MHC",
+        communityCity: "Elberta, AL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Florida",
     usStateId: "florida",
+    stateAbbr: "FL",
     communities: [
-      "Florida Community",
-      "Florida Community",
-      "Florida Community",
-      "Florida Community",
+      {
+        communityName: "Bay Oaks Village I & II",
+        communityCity: "Panama City, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Everglades MHP",
+        communityCity: "Clewiston, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Gulf Breeze MHC",
+        communityCity: "Hudson, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Journey's End",
+        communityCity: "Okeechobee, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Lake Wales MHC",
+        communityCity: "Lake Wales, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Oceanaire MHC",
+        communityCity: "New Smyrna Beach, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Ormond Beach Oasis",
+        communityCity: "Ormond Beach, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Palm Springs Estates",
+        communityCity: "Tarpon Springs, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Royal Oaks MHC",
+        communityCity: "Vero Beach, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Royal Palm Trailer Park",
+        communityCity: "Clewiston, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Town & Country MHC",
+        communityCity: "Jacksonville, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Westwood MHC",
+        communityCity: "Ormond Beach, FL",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
     ],
   },
   {
     state: "Indiana",
     usStateId: "indiana",
-    communities: ["Indiana Community", "Indiana Community"],
+    stateAbbr: "IN",
+    communities: [
+      {
+        communityName: "Moores Hill MHP",
+        communityCity: "Aurora, IN",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Kansas",
-    usStateId: "Kansas",
-    communities: ["Kansas Community", "Kansas Community"],
+    usStateId: "kansas",
+    stateAbbr: "KS",
+    communities: [
+      {
+        communityName: "Cherrywoods MHP",
+        communityCity: "Ottawa, KS",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Fishin' MHP",
+        communityCity: "Wichita, KS",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Kentucky",
     usStateId: "kentucky",
-    communities: ["Kentucky Community", "Kentucky Community"],
+    stateAbbr: "KY",
+    communities: [
+      {
+        communityName: "Ponderosa MHP",
+        communityCity: "Elizabethtown, KY",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Maryland",
     usStateId: "maryland",
-    communities: ["Maryland Community", "Maryland Community"],
+    stateAbbr: "MD",
+    communities: [
+      {
+        communityName: "Mobile Manor",
+        communityCity: "Rosedale, MD",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Rio Vista MHP",
+        communityCity: "Lothian, MD",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Massachusettes",
     usStateId: "massachusettes",
-    communities: ["Massachusettes Community", "Massachusettes Community"],
+    stateAbbr: "MA",
+    communities: [
+      {
+        communityName: "Mayflower Hills MHC",
+        communityCity: "Plymouth, MA",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Michigan",
     usStateId: "michigan",
-    communities: ["Michigan Community", "Michigan Community"],
+    stateAbbr: "MI",
+    communities: [
+      {
+        communityName: "Lamberton Lake MHC",
+        communityCity: "Grand Rapids, MI",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Victoria Woods MHC",
+        communityCity: "Midland, MI",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Kimball Estates",
+        communityCity: "Kimball, MI",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "New York",
     usStateId: "newyork",
-    communities: ["New York Community", "New York Community"],
+    stateAbbr: "NY",
+    communities: [
+      {
+        communityName: "Collins MHC",
+        communityCity: "Plattsburgh, NY",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Underwood Estates",
+        communityCity: "Plattsburgh, NY",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "North Carolina",
     usStateId: "northcarolina",
-    communities: ["North Carolina Community", "North Carolina Community"],
+    stateAbbr: "NC",
+    communities: [
+      {
+        communityName: "Cedar Grove MHC",
+        communityCity: "Kernersville, NC",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Colonial MHC",
+        communityCity: "New Bern, NC",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Lambeth MHC",
+        communityCity: "Winston-Salem, NC",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Parkwood MHC",
+        communityCity: "Winston-Salem, NC",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Vandemere MHC",
+        communityCity: "Greenville, NC",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "North Dakota",
     usStateId: "northdakota",
-    communities: ["North Dakota Community", "North Dakota Community"],
+    stateAbbr: "ND",
+    communities: [
+      {
+        communityName: "Apple Creek MHC",
+        communityCity: "Bismarck, ND",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Countryside MHC",
+        communityCity: "Mandan, ND",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Parktown MHC",
+        communityCity: "Mandan, ND",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Ohio",
     usStateId: "ohio",
-    communities: ["Ohio Community", "Ohio Community"],
+    stateAbbr: "OH",
+    communities: [
+      {
+        communityName: "Northgate MHP",
+        communityCity: "Mt Gilead, OH",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Oregon",
     usStateId: "oregon",
-    communities: ["Oregon Community", "Oregon Community"],
+    stateAbbr: "OR",
+    communities: [
+      {
+        communityName: "Shasta Glen MHC",
+        communityCity: "Klamath Falls, OR",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Valley View MHC",
+        communityCity: "Roseburg, OR",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Pennsylvania",
     usStateId: "pennsylvania",
-    communities: ["Pennsylvania Community", "Pennsylvania Community"],
+    stateAbbr: "PA",
+    communities: [
+      {
+        communityName: "Tracey Acres MHP",
+        communityCity: "Erie, PA",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+      {
+        communityName: "Tri-State MHP",
+        communityCity: "Erie, PA",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Texas",
     usStateId: "texas",
-    communities: ["Texas Community", "Texas Community"],
+    stateAbbr: "TX",
+    communities: [
+      {
+        communityName: "Knollwood Village",
+        communityCity: "Knollwood, TX",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "Washington",
     usStateId: "washington",
-    communities: ["Washinton Community", "Washington Community"],
+    stateAbbr: "WA",
+    communities: [
+      {
+        communityName: "University MHP",
+        communityCity: "Spokane Valley, WA",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
   {
     state: "West Virginia",
     usStateId: "westvirginia",
-    communities: ["West Virginia Community", "West Virginia Community"],
+    stateAbbr: "WV",
+    communities: [
+      {
+        communityName: "Cambridge MHC",
+        communityCity: "Charles Town, WV",
+        communityAddress: "Address",
+        communityType: "All Ages",
+        communityAmenities: "Amenities",
+      },
+    ],
   },
 ];
 
@@ -120,11 +436,11 @@ const Communities = () => {
                         <div class="card-body">
                           {/* COMMUNITY NAME */}
                           <h3 class="card-title community-card-title">
-                            {communities}
+                            {communities.communityName}
                           </h3>
                           {/* CITY/STATE OF THE COMMUNITY */}
                           <h5 className="card-title community-card-title">
-                            City, {communityStates.state}
+                            {communities.communityCity}
                           </h5>
                           {/* COMMUNITY IMAGE */}
                           {/* <img src="..." class="card-img-top" alt="..." /> */}
@@ -135,12 +451,12 @@ const Communities = () => {
                             <rect width="286" height="160" />
                           </svg>
                           {/* COMMUNITY ADDRESS */}
-                          <p class="card-text">Address</p>
-                          <p class="card-text">Type of Community</p>
-                          <p class="card-text">Amenities?</p>
+                          <p class="card-text">{communities.communityAddress}</p>
+                          <p class="card-text">Community Type: {communities.communityType}</p>
+                          <p class="card-text">{communities.communityAmenities}</p>
                           {/* LINK TO COMMUNITY PAGE */}
                           <a href="/communities" class="btn btn-primary">
-                            Details
+                            Learn More
                           </a>
                         </div>
                       </div>
