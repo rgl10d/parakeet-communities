40328e5205677db70ffdcc6aa55af2c314a1cbac
Garrett Lee <rgl10d@gmail.com>
2021-11-26 12:16:45 -0500

    added backend nodemailer connection to server.js and contact form

--- client/src/containers/Contact/Contact.jsx
+++ client/src/containers/Contact/Contact.jsx
@@ -1,42 +1,90 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Contact.css";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 
 const Contact = () => {
+  const [submitStatus, setSubmitStatus] = useState("Send");
+  const [mailerState, setMailerState] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  // INPUT FORM STATE CHANGE FUNCTION
+  function handleStateChange(e) {
+    setMailerState((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  }
+
+  const submitEmail = async (e) => {
+    e.preventDefault();
+    setSubmitStatus("Sending...");
+    await fetch("/contact", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ mailerState }),
+    })
+    .then((res) => res.json())
+    .then(async (res) => {
+      const resData = await res;
+      if(resData.status === "success") {
+        alert("Message Sent!");
+      } else if (resData.status === "fail") {
+        alert("Message failed to send!")
+      }
+    })
+    .then(() => {
+      setMailerState({
+        name: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+      setSubmitStatus("Send");
+    });
+  }
+
   return (
     <>
       <Navbar />
-
       <div id="contact-container" className="container">
-      <div id="contact-hero-image" alt="Man writing on paper" />
+        <div id="contact-hero-image" alt="Man writing on paper" />
         <h1 id="contact-hero-title">Send us a message</h1>
-        <form id="contact-form">
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputName">Name</label>
-              <input type="email" class="form-control" id="inputName" />
+        <form id="contact-form" onSubmit={submitEmail}>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="inputName">Name*</label>
+              <input required type="text" className="form-control" id="inputName" name="name" onChange={handleStateChange} />
             </div>
           </div>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputEmail">Email</label>
-              <input type="text" class="form-control" id="inputEmail" />
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="inputEmail">Email*</label>
+              <input required type="email" className="form-control" id="inputEmail" name="email" onChange={handleStateChange} />
             </div>
           </div>
-          <div class="form-row">
-            <div class="form-group col-md-4">
-              <label for="inputPhone">Phone Number</label>
-              <input type="text" class="form-control" id="inputPhone" />
+          <div className="form-row">
+            <div className="form-group col-md-4">
+              <label htmlFor="inputPhone">Phone Number</label>
+              <input type="text" className="form-control" id="inputPhone" name="phone" onChange={handleStateChange} />
             </div>
           </div>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputMessage">Message</label>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="inputMessage">Message*</label>
               <textarea
+                required
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="inputMessage"
+                name="message"
+                onChange={handleStateChange}
                 style={{ resize: "none", minHeight: "150px" }}
               />
             </div>
@@ -44,9 +92,9 @@ const Contact = () => {
           <button
             id="contact-send-button"
             type="submit"
-            class="btn btn-success"
+            className="btn btn-success"
           >
-            Send
+            {submitStatus}
           </button>
         </form>
       </div>
--- server.js
+++ server.js
@@ -1,27 +1,74 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const path = require("path");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 
 const PORT = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("client/build"));
 
-mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/parakeet-communities", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Successfully connected to database.");
-  })
-  .catch((err) => {
-    console.log("Unable to connect to database.");
-    console.log(err);
+// mongoose
+//   .connect(process.env.MONGODB_URI || "mongodb://localhost/parakeet-communities", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Successfully connected to database.");
+//   })
+//   .catch((err) => {
+//     console.log("Unable to connect to database.");
+//     console.log(err);
+//   });
+
+// MAIL TRANSPORTER OBJECT(CONTACT FORM EMAIL)
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "parakeet.communities.website@gmail.com",
+    pass: "comeFlyHome753",
+  },
+});
+
+// VERIFY CONTACT EMAIL CONNECTION
+transporter.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to send.");
+  }
+});
+
+// EMAIL TO SEND
+app.post("/contact", (req, res) => {
+  const name = req.body.mailerState.name;
+  const email = req.body.mailerState.email;
+  const phone = req.body.mailerState.phone;
+  const message = req.body.mailerState.message;
+  const mailOptions = {
+    from: name,
+    to: "parakeet.communities.website@gmail.com",
+    subject: "Contact Form Submission",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone Number: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      res.json({ status: "fail" });
+    } else {
+      console.log("== Message Sent ==");
+      res.json({
+        status: "success",
+      });
+    }
   });
+});
 
 app.get("/api/config", (req, res) => {
   res.json({
